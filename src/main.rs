8afcb4eceac2571b77599883f4f0c88f90b8b5ff
luo4lu <luo4lu@163.com>
2020-06-23 17:41:53 +0800

    add get quota information interface

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::env;
 mod config;
 mod dcds_regist_manage;
 mod meta_manage;
+mod get_quota_infor;
 pub mod response;
 
 #[actix_rt::main]
@@ -12,6 +13,12 @@ async fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     //Initialize the log and set the print level
     simple_logger::init_with_level(Level::Warn).unwrap();
+
+    let path = if args.len() > 1{
+        String::from(&args[1])
+    }else{
+        String::from("127.0.0.1:9001")
+    };
 
     HttpServer::new(|| {
         App::new()
@@ -23,8 +30,10 @@ async fn main() -> std::io::Result<()> {
             .service(dcds_regist_manage::dcds_reg_manage)
             .service(dcds_regist_manage::new_quota_manage)
             .service(dcds_regist_manage::get_dcds_allquota)
+            .service(get_quota_infor::chang_quota_state)
+            .service(get_quota_infor::get_quota_type)
     })
-    .bind(&args[1])?
+    .bind(path)?
     .run()
     .await
 }
