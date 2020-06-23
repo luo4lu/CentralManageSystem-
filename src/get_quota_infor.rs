390e8dfbeca31336ae298678aa87c998ced35f90
luo4lu <979375551@qq.com>
2020-06-23 18:13:27 +0800

    add get quota information interface (#4)
    * modified The address is passed through the command line
    
    * add get quota information interface

---
+++ src/get_quota_infor.rs
@@ -0,0 +1,232 @@
+use crate::response::ResponseBody;
+use actix_web::{post,get, web, HttpResponse, Responder};
+use log::{info, warn};
+use serde::{Deserialize, Serialize};
+//数据库相关
+use deadpool_postgres::Pool;
+use tokio_postgres::row::Row;
+
+//修改货币发行状态
+#[derive(Deserialize, Debug)]
+pub struct QuotaApproveRequest{
+    aid: String,
+    extra: serde_json::Value,
+    state: String,
+}
+
+#[post("/api/quota/approve")]
+pub async fn chang_quota_state(
+    data: web::Data<Pool>,
+    qstr: web::Json<QuotaApproveRequest>,
+) -> impl Responder{
+    //连接数据库句柄
+    let conn = data.get().await.unwrap();
+    //state值判断
+    if (qstr.state == "approve") || (qstr.state == "registe") || (qstr.state == "deined"){
+        info!("input state :{:?}", qstr.state);
+    }else{
+        warn!("request state error:{:?} ,please input the correct state",qstr.state);
+        return HttpResponse::Ok().json(ResponseBody::<()>::new_json_parse_error());
+    }
+    //修改数据表的值得状态
+    match conn
+        .query("UPDATE quota_admin SET extra = $1,state = $2,update_time = now() where aid = $3",
+    &[&qstr.extra,&qstr.state,&qstr.aid]).await
+    {
+        Ok(row) => {
+            info!("update success!{:?}", row);
+            row
+        }
+        Err(error) => {
+            warn!("chang_quota_state update failde!!{:?}", error);
+            return HttpResponse::Ok().json(ResponseBody::<String>::database_runing_error(Some(error.to_string())));
+        }
+    };
+    HttpResponse::Ok().json(ResponseBody::<()>::new_success(None))
+}
+
+//获取额度控制表中不同状态额度值
+#[derive(Deserialize, Debug)]
+pub struct StatesRequest {
+    aid: String,
+}
+
+#[derive(Serialize)]
+pub struct StatesRespones{
+    tatol:i64,   //总额度（额度管理表中状态为delivery的value和）
+    issued:i64,  //已发行额度（额度发行表中info信息字段和）
+    waitly:i64,   //待发放额度tatol-issued
+    recycle:i64   //已回收额度 withdraw
+}
+
+#[get("/api/quota/statistics")]
+pub async fn get_quota_type(
+    data: web::Data<Pool>,
+    req: web::Json<StatesRequest>,
+) -> impl Responder{
+    //连接数据库句柄
+    let conn = data.get().await.unwrap();
+    let mut tatol:i64 = 0;   //总额度（额度管理表中状态为delivery的value和）
+    let mut issued:i64 = 0;  //已发行额度（额度发行表中info信息字段和）
+    let mut waitly:i64 = 0;   //待发放额度tatol-issued
+    let mut recycle:i64 = 0;   //已回收额度 withdraw
+    let mut select_tatol:Vec<Row> = Vec::new();
+    let mut select_issued:Vec<Row> = Vec::new();
+    let mut select_recycle:Vec<Row> = Vec::new();
+    let type_delive = String::from("delivery");
+    let type_with = String::from("withdraw");
+//    let aid = Some(&req.aid);
+//    if let Some(id) = aid{
+    if req.aid.len() > 5{
+        //总额度查询
+        select_tatol = match conn
+            .query("SELECT value from quota_admin where aid = $1 AND type = $2",
+        &[&req.aid,&type_delive]).await{
+            Ok(row) => {
+                info!("electe success: {:?}", row);
+                row
+            }
+            Err(error) => {
+                warn!("select failed :{:?}!!", error);
+                return HttpResponse::Ok().json(ResponseBody::<String>::database_runing_error(
+                    Some(error.to_string()),
+                ));
+            }
+        };
+        if select_tatol.is_empty() {
+            warn!("1.SELECT check get_quota_type failed,please check get_quota_type value");
+            return HttpResponse::Ok().json(ResponseBody::<()>::database_build_error());
+        }
+        //已发额度查询
+        select_issued = match conn
+            .query("SELECT (issue_info->'t_obj'->'issue_info') from quota_delivery where aid = $1",
+        &[&req.aid]).await{
+            Ok(row) => {
+                info!("electe success: {:?}", row);
+                row
+            }
+            Err(error) => {
+                warn!("select failed :{:?}!!", error);
+                return HttpResponse::Ok().json(ResponseBody::<String>::database_runing_error(
+                    Some(error.to_string()),
+                ));
+            }
+        };
+        if select_issued.is_empty() {
+            warn!("2.SELECT check get_quota_type failed,please check get_quota_type value");
+            return HttpResponse::Ok().json(ResponseBody::<()>::database_build_error());
+        }
+        //回收额度
+        select_recycle = match conn
+            .query("SELECT value from quota_admin where aid = $1 AND type = $2",
+        &[&req.aid,&type_with]).await{
+            Ok(row) => {
+                info!("electe success: {:?}", row);
+                row
+            }
+            Err(error) => {
+                warn!("3.select failed :{:?}!!", error);
+                return HttpResponse::Ok().json(ResponseBody::<String>::database_runing_error(
+                    Some(error.to_string()),
+                ));
+            }
+        };
+        if select_recycle.is_empty() {
+            warn!("3.SELECT check get_quota_type The amount recovered is empty");
+          //  return HttpResponse::Ok().json(ResponseBody::<()>::database_build_error());
+        }
+    }else{
+        //总额度查询
+        select_tatol = match conn
+            .query("SELECT value from quota_admin where type = $1",
+        &[&type_delive]).await{
+            Ok(row) => {
+                info!("electe success: {:?}", row);
+                row
+            }
+            Err(error) => {
+                warn!("4.select failed :{:?}!!", error);
+                return HttpResponse::Ok().json(ResponseBody::<String>::database_runing_error(
+                    Some(error.to_string()),
+                ));
+            }
+        };
+        if select_tatol.is_empty() {
+            warn!("4.SELECT check get_quota_type failed,please check get_quota_type value");
+            return HttpResponse::Ok().json(ResponseBody::<()>::database_build_error());
+        }
+        //已发额度查询
+        select_issued = match conn
+            .query("SELECT (issue_info->'t_obj'->'issue_info') from quota_delivery where $1 !='' ",
+        &[&"aid"]).await{
+            Ok(row) => {
+                info!("electe success: {:?}", row);
+                row
+            }
+            Err(error) => {
+                warn!("5.select failed :{:?}!!", error);
+                return HttpResponse::Ok().json(ResponseBody::<String>::database_runing_error(
+                    Some(error.to_string()),
+                ));
+            }
+        };
+        if select_issued.is_empty() {
+            warn!("5.SELECT check get_quota_type failed,please check get_quota_type value");
+            return HttpResponse::Ok().json(ResponseBody::<()>::database_build_error());
+        }
+        //回收额度
+        select_recycle = match conn
+            .query("SELECT value from quota_admin where type = $1",
+        &[&type_with]).await{
+            Ok(row) => {
+                info!("electe success: {:?}", row);
+                row
+            }
+            Err(error) => {
+                warn!("select failed :{:?}!!", error);
+                return HttpResponse::Ok().json(ResponseBody::<String>::database_runing_error(
+                    Some(error.to_string()),
+                ));
+            }
+        };
+        if select_recycle.is_empty() {
+            warn!("6.SELECT check get_quota_type The amount recovered is empty");
+           // return HttpResponse::Ok().json(ResponseBody::<()>::database_build_error());
+        }
+    }
+    //获取总额度
+    for item in select_tatol.iter(){
+        let value: i64 = item.get(0);
+        tatol += value;
+    }
+    //获取已回收额度
+    for item in select_recycle.iter(){
+        let value: i64 = item.get(0);
+        recycle += value;
+    }
+    //获取已发放额度总和
+    for item in select_issued.iter(){
+        let issued_vec: Vec<Vec<i64>> = serde_json::from_value(item.get(0)).unwrap();
+        for vec_num in issued_vec.iter(){
+            let mut number:Vec<i64> = [0;2].to_vec();
+            for (index,quota) in vec_num.iter().enumerate().take(number.len()){
+                number[index] = *quota;
+            }
+            issued += number[0]*number[1];
+        }
+    }
+    //获取待发放额度
+    if tatol >= issued{
+        waitly = tatol - issued;
+    }else{
+        warn!("The amount issued should not be greater than the total amount!!");
+        return HttpResponse::Ok().json(ResponseBody::<()>::new_json_parse_error());
+    }
+    let all_quota = StatesRespones{
+        tatol, 
+        issued,  
+        waitly,   
+        recycle,
+    };
+    HttpResponse::Ok().json(ResponseBody::<StatesRespones>::new_success(Some(all_quota)))
+}
